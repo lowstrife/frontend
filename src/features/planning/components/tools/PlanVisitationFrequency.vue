3e561edb10b60d6a548034430d46e014d78cbc85
Jan <jplacht@gmail.com>
2025-05-11 09:15:45 +0200

    Prepare plan tool async component loading

---
+++ src/features/planning/components/tools/PlanVisitationFrequency.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+
+    import { NButton } from 'naive-ui';
+    import { CloseSharp } from '@vicons/material';
+
+    const props = defineProps({
+        amountBuildings: {
+            type: Number,
+            required: true,
+        }
+    });
+
+        const emit = defineEmits<{
+            (e: "close"): void;
+        }>();
+
+    await new Promise<void>(resolve => setTimeout(resolve, 2_000))
+
+</script>
+
+<template>
+
+    <div class="pb-3 flex flex-row justify-between child:my-auto">
+        <h2 class="text-white/80 font-bold text-lg">Visitation Frequency</h2>
+        <n-button size="tiny" secondary @click="emit('close')">
+            <template #icon><CloseSharp /></template>
+        </n-button>
+    </div>
+
+    Meow! Got id {{ amountBuildings }}
+
+
+    <br />
+    <br />
+    <div>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</div>
+    <br />
+    <div>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</div>
+    <br />
+    <div>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</div>
+    <br />
+    <div>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</div>
+    <br />
+    <div>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</div>
+    
+
+
+</template>
--- src/views/PlanView.vue
+++ src/views/PlanView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-	import { PropType, ref, type Ref } from "vue";
+	import { computed, defineAsyncComponent, nextTick, PropType, ref, type Ref } from "vue";
 
 	// Types & Interfaces
 	import { IPlan, IPlanEmpireElement } from "@/stores/planningStore.types";
@@ -26,7 +26,7 @@
 	import PlanConfiguration from "@/features/planning/components/PlanConfiguration.vue";
 
 	// UI
-	import { NButton, NIcon, NTooltip } from "naive-ui";
+	import { NButton, NIcon, NTooltip, NSpin } from "naive-ui";
 
 	import {
 		AutoAwesomeMosaicOutlined,
@@ -104,6 +104,35 @@
 		refEmpireUuid.value = props.empireList[0].uuid;
 		refCXUuid.value = findEmpireCXUuid(refEmpireUuid.value);
 	}
+
+	/**
+	 * Tool Setup
+	 */
+
+	type toolOptions = 'visitation-frequency' | null;
+
+	const refShowTool: Ref<toolOptions> = ref(null);
+
+	function openTool(key: toolOptions): void {
+		refShowTool.value = null;
+		nextTick(() => {
+			refShowTool.value = key;
+		})
+	}
+
+	const compViewToolMeta = computed(() => {
+		switch (refShowTool.value) {
+			case "visitation-frequency": 
+				return {
+					component: defineAsyncComponent(() => import("@/features/planning/components/tools/PlanVisitationFrequency.vue")),
+					props: {amountBuildings: result.value.production.buildings.length},
+					listeners: {},
+				}
+			default:
+				return null
+		}
+	});
+
 </script>
 
 <template>
@@ -222,7 +251,7 @@
 				</div>
 				<div class="flex-grow text-end my-auto pr-6 font-bold">Tools</div>
 				<div class="flex justify-end gap-x-3">
-					<n-button size="small" secondary>Empire Override</n-button>
+					<n-button size="small" secondary @click="openTool('visitation-frequency')">Empire Override</n-button>
 					<n-button size="small" secondary>POPR</n-button>
 					<n-button size="small" secondary>Visitation Frequency</n-button>
 					<n-button size="small" secondary>Construction Cart</n-button>
@@ -230,6 +259,27 @@
 					<n-button size="small" secondary>Repair Analysis</n-button>
 					<n-button size="small" secondary>Habitation Optimization</n-button>
 				</div>
+			</div>
+			<div 
+				:class="!refShowTool ? 'opacity-0 overflow-hidden !h-0' : 'px-6 py-3 opacity-100 border-b border-white/5'" 
+				class="transition-discrete transition-opacity duration-500 bg-gray-dark"
+			>
+				<Suspense v-if="refShowTool && compViewToolMeta">
+					<template #default>
+						<component
+						:is="compViewToolMeta.component"
+						:key="refShowTool"
+						v-bind="compViewToolMeta.props"
+						v-on="compViewToolMeta.listeners"
+						v-on:close="() => refShowTool = null"
+						/>
+					</template>
+					<template #fallback>
+						<div class="w-full text-center py-5">
+							<n-spin size="small" />
+						</div>
+					</template>
+				</Suspense>
 			</div>
 			<div class="p-6 grid grid-cols-1 lg:grid-cols-[auto_450px] gap-6">
 				<div>
